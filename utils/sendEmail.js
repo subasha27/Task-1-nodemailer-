17f564e745aec1238d85134325c03941f14e0dbb
subasha27 <68815694+subasha27@users.noreply.github.com>
2023-08-10 16:04:47 +0530

    updated

--- utils/sendEmail.js
+++ utils/sendEmail.js
@@ -1,7 +1,21 @@
 const nodemailer = require("nodemailer");
 
-const sendEmail = async (email, subject, text) => {
+const sendEmail = async (mail, subject, text) => {
     try {
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+              type: 'OAuth2',
+              user: "subash.doodleblue@gmail.com",
+              pass: "samsub007",
+              clientId: "755656838372-7n3uafk51o3ou79mumdq151gl13ige3s.apps.googleusercontent.com",
+              clientSecret: "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",
+              refreshToken: "1//04bj_7IeBCWudCgYIARAAGAQSNwF-L9IrBbRrx9UTm8HOtaDF4ZvZuJyG0lwk3C1sspOuEzJTxMlmTaa0ZM5bSG7SxxcPXGc4dEc"
+            }
+          });
+
+          //mail trap
+          /* try {
         const transporter = nodemailer.createTransport({
             host: "smtp.mailtrap.io",
     port: 2525,
@@ -9,11 +23,12 @@ const sendEmail = async (email, subject, text) => {
         user: "70435e492cce35",//mailtrap id
         pass: "ec3fbd15c35860" 
        }
-    });
+    });*/
+
 
         await transporter.sendMail({
-            from: process.env.USER,
-            to: email,
+            from: "subash.doodleblue@gmail.com",
+            to: mail,
             subject: subject,
             text: text,
         });
